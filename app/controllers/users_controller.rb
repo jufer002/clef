34f1d2605b181c2a00cd62f63dc8c1598a1efc75
jufer002 <jufer002@mail.goucher.edu>
2020-03-01 12:22:23 -0500

    Set up some validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,7 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:username, :email, :password_digest)
+      params.require(:user).permit(:username, :email, :password, 
+                                  :password_confirmation)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :lessons
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   root 'static_pages#home'
+  get '/signup', to: 'users#new'
 end
