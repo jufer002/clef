b2c9f68062b9c90274fbdab1b6328a9b61883a72
jufer002 <jufer002@mail.goucher.edu>
2020-03-01 12:39:46 -0500

    Sign up works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,14 +26,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      flash[:success] = "Welcome to Clef!"
+      # Log in
+      # ...
+      # Redirect home
+      redirect_to root_path
+    else
+      render 'new'
     end
   end
 
