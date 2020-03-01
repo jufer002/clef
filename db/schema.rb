c1700adb161ea2ae34ffebcf2555f74d60eba736
jufer002 <jufer002@mail.goucher.edu>
2020-03-01 12:01:54 -0500

    placeholder homepage is here

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_28_193609) do
+ActiveRecord::Schema.define(version: 2020_02_28_200300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lessons", force: :cascade do |t|
+    t.string "title"
+    t.string "text"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
