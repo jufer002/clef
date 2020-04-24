7c701b2f9fe8ca6ea2b600199e08544b71addabc
jufer002 <jufer002@mail.goucher.edu>
2020-04-24 12:25:18 -0400

    Beginning work on fancy new course page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,8 @@ class SearchController < ApplicationController
     @lessons = []
     if !params[:search_text].blank?
       @lessons << Lesson.where("title LIKE ?", "%#{params[:search_text]}%")
+      # Because ActionText stores rich text in a separate table, we need to join before filtering by
+      # the lesson body.
       @lessons << Lesson.joins("INNER JOIN action_text_rich_texts ON action_text_rich_texts.record_id = lessons.id")
         .where("action_text_rich_texts.body LIKE ?", "%#{params[:search_text]}%")
       
