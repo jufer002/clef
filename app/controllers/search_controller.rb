56a3b43f2b40c844ee2a8c68f9faf2ca0f06eb85
fernandoaestrella <fernandoaestrella@gmail.com>
2020-04-25 13:54:54 -0400

    added course search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,7 @@ class SearchController < ApplicationController
 
   def search
     @lessons = []
+    @courses = []
     if !params[:search_text].blank?
       @lessons << Lesson.where("title LIKE ?", "%#{params[:search_text]}%")
       # Because ActionText stores rich text in a separate table, we need to join before filtering by
@@ -9,9 +10,16 @@ class SearchController < ApplicationController
       @lessons << Lesson.joins("INNER JOIN action_text_rich_texts ON action_text_rich_texts.record_id = lessons.id")
         .where("action_text_rich_texts.body LIKE ?", "%#{params[:search_text]}%")
       
-      if @lessons[0].empty? && @lessons[1].empty?
-        @lessons = []
-      end
+        
+        if @lessons[0].empty? && @lessons[1].empty?
+          @lessons = []
+        end
+
+        @courses << Course.where("title LIKE ?", "%#{params[:search_text]}%")
+
+        if @courses[0].empty?
+          @courses = []
+        end
     end
   end
 end
